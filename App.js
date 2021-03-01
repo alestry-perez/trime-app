3bc5161a622d9d9f111fb754bb31b7e6ce3e24b0
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-01 17:37:15 +0100

    commit

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView,
+  View,
+  InteractionManager,
+} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View style={styles.mainContainer}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/trime-tag.png')} />
+      <Image source={require('./assets/trime-subtext.png')} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    padding: 30,
-    backgroundColor: 'dodgerblue',
-  },
   container: {
     flex: 1,
+    resizeMode: 'cover',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
