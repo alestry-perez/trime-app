34bad546edcc825661bb249ac3acaba4aeebb814
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-01 16:48:14 +0100

    commit

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.mainContainer}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 30,
+    backgroundColor: 'dodgerblue',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
