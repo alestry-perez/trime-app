37bcf561b5f2207c038db504aa82bd2c8e3dd66e
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-02 01:30:31 +0100

    commit

--- App.js
+++ App.js
@@ -1,37 +1,6 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  Image,
-  SafeAreaView,
-  View,
-  InteractionManager,
-  Button,
-  Platform,
-  StatusBar,
-} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ height: 300 }}>
-        <Image source={require('./assets/trime-tag.png')} />
-      </View>
-      <View style={{ height: 300 }}>
-        <Image source={require('./assets/trime-subtext.png')} />
-      </View>
-      <Button title="Start" onPress={() => console.log('Button Tapped')} />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    resizeMode: 'cover',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === ['ios, android'] ? StatusBar.currentHeight : 0,
-  },
-});
---
+++ app/config/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: 'dodgerblue',
+  secondary: 'red',
+  black: '#000',
+  white: '#fff',
+};
