564229a4f4f55162f10562b5a5d511dacadfec25
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-02 20:19:06 +0100

    commit

--- app/screens/LoginPage.js
+++ app/screens/LoginPage.js
@@ -6,48 +6,59 @@ import {
   Button,
   Platform,
   StatusBar,
+  SafeAreaView,
 } from 'react-native';
 
 import colors from '../config/colors';
 
 function LoginPage(props) {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.background}
-        source={require('../assets/background.png')}
-      />
-      <View style={styles.buttons}>
-        <View style={styles.abutton}>
-          <Button
-            onPress={() => console.log('Button Tapped')}
-            title="Continue with Apple"
-          />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Image
+          style={styles.background}
+          source={require('../assets/background.png')}
+        />
+        <View style={styles.buttons}>
+          <View style={styles.abutton}>
+            <Button
+              color="black"
+              onPress={() => console.log('Button Tapped')}
+              title="Continue with Apple"
+            />
+          </View>
+          <View style={styles.fbutton}>
+            <Button
+              color="white"
+              onPress={() => console.log('Button Tapped')}
+              title="Continue with Facebook"
+            />
+          </View>
+          <View style={styles.gbutton}>
+            <Button
+              color="black"
+              onPress={() => console.log('Button Tapped')}
+              title="Continue with Google"
+            />
+          </View>
+          <View style={styles.ebutton}>
+            <Button
+              color="white"
+              onPress={() => console.log('Button Tapped')}
+              title="Continue with Email"
+            />
+          </View>
         </View>
-        <View style={styles.gbutton}>
-          <Button
-            onPress={() => console.log('Button Tapped')}
-            title="Continue with Google"
-          />
-        </View>
-        <View style={styles.fbutton}>
-          <Button
-            onPress={() => console.log('Button Tapped')}
-            title="Continue with Facebook"
-          />
-        </View>
-        <View style={styles.ebutton}>
-          <Button
-            onPress={() => console.log('Button Tapped')}
-            title="Continue with Email"
-          />
-        </View>
+        <Image
+          style={styles.subtext}
+          source={require('../assets/sub-text.png')}
+        />
+        <Image
+          style={styles.loginpic}
+          source={require('../assets/trime-login-pic.png')}
+        />
       </View>
-      <Image
-        style={styles.loginpic}
-        source={require('../assets/trime-login-pic.png')}
-      />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -61,14 +72,16 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === ['ios, android'] ? StatusBar.currentHeight : 0,
   },
   background: {
-    top: 315,
+    top: 350,
   },
   loginpic: {
-    top: -675,
+    top: -750,
   },
   buttons: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
+    top: -90,
+  },
+  subtext: {
+    top: -50,
   },
   abutton: {
     backgroundColor: colors.white,
