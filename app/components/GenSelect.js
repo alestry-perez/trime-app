ad83cef08f17a39929c99e3a6de177e05c63f936
Alestry Pérez <alestry.perez@hyperisland.se>
2021-03-11 14:09:29 +0100

    commit

--- app/components/GenSelect.js
+++ app/components/GenSelect.js
@@ -7,69 +7,67 @@ export let GenSelect = () => {
     setSelected(!selected);
   };
   return (
-    <View style={styles.container}>
-      <View style={styles.genIcon}>
-        <TouchableHighlight
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 10,
-            height: 70,
-            width: 70,
-            backgroundColor: selected ? '#FFB424' : '#E8E8E8',
-          }}
-          activeOpacity={0.6}
-          underlayColor=""
-          onPress={onPress}
-        >
-          <Image source={require('../assets/genX.png')} />
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 10,
-            height: 70,
-            width: 70,
-            backgroundColor: selected ? '#FFB424' : '#E8E8E8',
-          }}
-          activeOpacity={0.6}
-          underlayColor=""
-          onPress={onPress}
-        >
-          <Image source={require('../assets/gen.png')} />
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 10,
-            height: 70,
-            width: 70,
-            backgroundColor: selected ? '#FFB424' : '#E8E8E8',
-          }}
-          activeOpacity={0.6}
-          underlayColor=""
-          onPress={onPress}
-        >
-          <Image source={require('../assets/genY.png')} />
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 10,
-            height: 70,
-            width: 70,
-            backgroundColor: selected ? '#FFB424' : '#E8E8E8',
-          }}
-          activeOpacity={0.6}
-          underlayColor=""
-          onPress={onPress}
-        >
-          <Image source={require('../assets/genO.png')} />
-        </TouchableHighlight>
-      </View>
+    <View style={styles.genIcon}>
+      <TouchableHighlight
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 10,
+          height: 70,
+          width: 70,
+          backgroundColor: selected ? '#FFB424' : '#E8E8E8',
+        }}
+        activeOpacity={0.6}
+        underlayColor=""
+        onPress={onPress}
+      >
+        <Image source={require('../assets/genX.png')} />
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 10,
+          height: 70,
+          width: 70,
+          backgroundColor: selected ? '#FFB424' : '#E8E8E8',
+        }}
+        activeOpacity={0.6}
+        underlayColor=""
+        onPress={onPress}
+      >
+        <Image source={require('../assets/gen.png')} />
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 10,
+          height: 70,
+          width: 70,
+          backgroundColor: selected ? '#FFB424' : '#E8E8E8',
+        }}
+        activeOpacity={0.6}
+        underlayColor=""
+        onPress={onPress}
+      >
+        <Image source={require('../assets/genY.png')} />
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 10,
+          height: 70,
+          width: 70,
+          backgroundColor: selected ? '#FFB424' : '#E8E8E8',
+        }}
+        activeOpacity={0.6}
+        underlayColor=""
+        onPress={onPress}
+      >
+        <Image source={require('../assets/genO.png')} />
+      </TouchableHighlight>
     </View>
   );
 };
--- app/components/Tagx.js
+++ app/components/Tagx.js
@@ -1,30 +1,42 @@
-import {Text, TouchableOpacity, View} from "react-native";
-import React, {useState} from "react";
+import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 
-function SupiTag({value, style}) {
-    let [selected, setSelected] = useState(false);
-    const onPress = () => {
-        setSelected(!selected)
-    }
-
-    return <TouchableOpacity onPress={onPress}><View style={style}><Text style={{
-        fontSize: 15,
-        fontWeight: 'bold',
-        padding: 10,
-        color: selected ? 'black' : 'white',
-        backgroundColor: selected ? 'white' : 'black'
-    }}>{value}</Text></View></TouchableOpacity>
+function SupiTag({ value, style }) {
+  let [selected, setSelected] = useState(false);
+  const onPress = () => {
+    setSelected(!selected);
+  };
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={style}>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: 'bold',
+            padding: 10,
+            color: selected ? 'black' : 'white',
+            backgroundColor: selected ? 'white' : 'black',
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
-export function TagsX({initialTags, style}) {
-
-    return <View style={{
+export function TagsX({ initialTags, style }) {
+  return (
+    <View
+      style={{
         flex: 1,
         flexDirection: 'row',
-        flexWrap: 'wrap'
-    }}>
-        {initialTags.map(tag => {
-            return <SupiTag key={tag} value={tag} style={style}/>
-        })}
+        flexWrap: 'wrap',
+      }}
+    >
+      {initialTags.map((tag) => {
+        return <SupiTag key={tag} value={tag} style={style} />;
+      })}
     </View>
+  );
 }
